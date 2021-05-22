7a5f9c37511b3de08eaecde00891165621967c8a
Bogdan <bogdanbondarenko19@yandex.ru>
2021-05-23 01:21:48 +0300

    First commit

---
+++ safety_doors/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace safety_doors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "cloud_0_0003.pcd";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Путь к .pcd:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Определить безопасность";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 39);
+            this.label2.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Сохранить вид сверху с прямыми";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 199);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Безопасные двери";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ safety_doors/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace safety_doors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Position> readPCD()
+        {
+            char[] sep = new char[1] { ' ' };
+            List<Position> pos = new List<Position>();
+            StreamReader sr = new StreamReader(textBox1.Text);
+            for (int i = 0; i < 11; i++)
+                sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] strs = sr.ReadLine().Replace('.', ',').Split(sep);
+                double x = double.Parse(strs[0]);
+                double y = double.Parse(strs[1]);
+                double z = double.Parse(strs[2]);
+                pos.Add(new Position(x, y, z));
+            }
+            return pos;
+        }
+
+        //вид сверху с прямыми
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<Position> list = readPCD();
+            Bitmap image = new Bitmap(4000, 4000);
+            for (int i = 0; i < list.Count; i++)
+            {
+                Position current = list[i];
+                image.SetPixel((int)((current.x + 2) * 1000), (int)((current.y + 2) * 1000), Color.FromArgb(0, 0, (int)(current.z * 32)));
+            }
+            Graphics g1 = Graphics.FromImage(image);
+            Pen pen = new Pen(Color.Red, 5);
+            g1.DrawLine(pen, 0, (float)y(-2) * 1000 + 2000, 4000, (float)y(2) * 1000 + 2000);
+            g1.DrawLine(pen, 0, (float)y1(-2) * 1000 + 2000, 4000, (float)y1(2) * 1000 + 2000);
+            g1.DrawLine(pen, 0, (float)y2(-2) * 1000 + 2000, 4000, (float)y2(2) * 1000 + 2000);
+            image.Save(textBox1.Text.Replace(".pcd", "_LINES.png"));
+        }
+
+        //определить безопасность
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<Position> list = readPCD();
+
+            const int minCountDetected = 40; //минимальное количество для утверждения опасности
+            int countDetected = 0; //количество точек в плоскости
+            for (int i = 0; i < list.Count; i++)
+                if (inspect(list[i]))
+                    countDetected++;
+
+            bool safety = countDetected < minCountDetected;
+            label2.Text = safety ? "Безопасно" : "Опасно";
+            label2.ForeColor = safety ? Color.Lime : Color.Red;
+        }
+
+        private const double baseOffset = 0.28; //смещение плоскости от вертикальной оси
+        private const double inspectWeight = 0.04; //толщина плоскости
+
+        //прямая - центр плоскости
+        private double y(double x)
+        {
+            return -x / 12.4 - baseOffset;
+        }
+
+        //прямая - нижняя граница плоскости по Y
+        private double y1(double x)
+        {
+            return -x / 12.4 - baseOffset - inspectWeight / 2;
+        }
+
+        //прямая - верхняя граница плоскости по Y
+        private double y2(double x)
+        {
+            return -x / 12.4 - baseOffset + inspectWeight / 2;
+        }
+
+        //точка находится внутри инспектируемой плоскости
+        private bool inspect(Position pos)
+        {
+            return pos.y > y1(pos.x) && pos.y < y2(pos.x);
+        }
+    }
+
+    //3D точка
+    public class Position
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        public Position(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
